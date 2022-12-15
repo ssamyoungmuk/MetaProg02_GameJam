9c33161db98d562d08fddc7c758d9e739fcbe178
efrgdc <coinus@naver.com>
2022-12-15 16:55:45 +0900

    UImake

--- Assets/Team_6/Scripts/GameManager_06.cs
+++ Assets/Team_6/Scripts/GameManager_06.cs
@@ -6,12 +6,30 @@ using UnityEngine.UI;
 public class GameManager_06 : MonoBehaviour
 {
     public float timer;
+    public int money = 0;
+
+    Monster_Move_06 monsterMove = null;
+
+    int stage = 1;
+    
+
+    public Text myMoney;
     public Text text_Timer;
 
+    private void Start()
+    {
+        monsterMove = GetComponent<Monster_Move_06>();
+        myMoney = GameObject.Find("Money").GetComponent<Text>();
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
         text_Timer.text = "TIME : " + Mathf.Round(timer);
+    }
+
+    void Setmoney()
+    {
+        myMoney.text = "Money : " + money.ToString();
     }
 }
