5d43b4dbd2e716124094cf33ca788dd5ed83a1cc
efrgdc <coinus@naver.com>
2022-12-15 14:39:56 +0900

    MOnsterHpbar
    hp쓰기

--- Assets/Team_6/Scripts/Monster.cs
+++ Assets/Team_6/Scripts/Monster.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     [SerializeField] GameObject monsterPrefab;
+    [SerializeField] GameObject monsterPrefab2;
     public List<GameObject> monsterList = new List<GameObject>();
+    public List<GameObject> monsterList2 = new List<GameObject>();
     public int index = 0;
-
-    float posX = 15;
-    float posY = 0.5f;
+    public int index2 = 0;
 
     float spawnTime = 0f;
+    float spawnTime2 = 0f;
 
 
     private void Awake()
@@ -26,6 +27,17 @@ public class Monster : MonoBehaviour
             monster.gameObject.SetActive(false);
             monsterList.Add(monster);
         }
+
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject monster = Instantiate(monsterPrefab2);
+            monster.transform.position = new Vector3(15, 0.5f, 0);
+            monster.transform.rotation = Quaternion.identity;
+            monster.transform.localScale = Vector3.one;
+
+            monster.gameObject.SetActive(false);
+            monsterList2.Add(monster);
+        }
     }
 
     
@@ -33,11 +45,17 @@ public class Monster : MonoBehaviour
     void Update()
     {
         spawnTime += Time.deltaTime;
+        spawnTime2 += Time.deltaTime;
+       
         if (spawnTime >= 2)
         {
             MonsterComing();
         }
-        
+        if (spawnTime2 >= 3)
+        {
+            MonsterComing2();
+        }
+
     }
 
     void MonsterComing()
@@ -48,5 +66,15 @@ public class Monster : MonoBehaviour
         index++;
 
         spawnTime = 0f;
+    }
+
+    void MonsterComing2()
+    {
+        if (index2 >= monsterList2.Count) index2 = 0;
+        monsterList2[index2].SetActive(true);
+
+        index2++;
+
+        spawnTime2 = 0f;
     }
 }
--- Assets/Team_6/Scripts/Monster_Move.cs
+++ Assets/Team_6/Scripts/Monster_Move.cs
@@ -1,27 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Monster_Move : MonoBehaviour
 {
+    [SerializeField] float monsterHP;
+    float monsterMaxHP = 0;
     float speed = 10f;
+    public Image hpBar;
+
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-        StartCoroutine(MonsterDie());
+        StartCoroutine(MonsterDie());  // 삭제할것
     }
 
-    IEnumerator MonsterDie()
+    IEnumerator MonsterDie()  // 트리거 만들고나면 삭제
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(4f);
 
         transform.position = new Vector3(15, 0.5f, 0);
         transform.rotation = Quaternion.identity;
         transform.localScale = Vector3.one;
 
         gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.CompareTag("Stone"))
+        {
+            monsterHP--;
+
+            hpBar.rectTransform.sizeDelta = new Vector2(monsterHP, 0.1f);
+
+            if(monsterHP <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
