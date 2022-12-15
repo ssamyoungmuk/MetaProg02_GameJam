078dda2b60fe85dc1876f08364799d0f77f385cc
efrgdc <coinus@naver.com>
2022-12-15 17:45:27 +0900

    Prefab

--- Assets/Team_6/Scripts/GameManager_06.cs
+++ Assets/Team_6/Scripts/GameManager_06.cs
@@ -5,25 +5,26 @@ public class GameManager_06 : MonoBehaviour
 {
     public float timer;
     public int money = 0;
+    public int stage = 1;
 
     Monster_Move_06 monsterMove = null;
 
-    int stage = 1;
-    
-
     public Text myMoney;
-    public Text text_Timer;
+    public Text myTimer;
+    public Text myStage;
 
-    Player_06 player = FindObjectOfType<Player_06>();
+    Player_06 player = null;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player_06>();
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
-        text_Timer.text = "TIME : " + Mathf.Round(timer);
-    }
-
-    void Setmoney()
-    {
+        myTimer.text = "TIME : " + Mathf.Round(timer);
         myMoney.text = "Money : " + money.ToString();
+        myStage.text = "Stage : " + stage.ToString();
     }
 }
