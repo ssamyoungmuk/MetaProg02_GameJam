0c0a4af51e7017920fca461ca585395f39c0ed17
HyesungKum <rmagptjd77@gmail.com>
2022-12-16 10:13:59 +0900

    fixed playerprefabs

--- Assets/OneOnOne/Script/Weapon.cs
+++ Assets/OneOnOne/Script/Weapon.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 namespace OOO
 {
-   
+    public class Weapon : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log(collision.gameObject.name.Substring(0,9));
+            //if()
+        }
+    }
 }
 
