73ece078ea175031252a79d3495976bc1c9e2f27
chaeza <chaezaroad@gmail.com>
2022-12-16 15:15:31 +0900

    Revert "TeamNumber5"
    This reverts commit abec1e5d3116c1667cb01a25e6c5606bc62f663e, reversing
    changes made to 12b50cf72e5ef27b285b927fe2e66b80c453f33a.

--- Assets/Scripts/GameFrame.cs
+++ Assets/Scripts/GameFrame.cs
@@ -7,13 +7,11 @@ using System;
 
 public class GameFrame : MonoBehaviour
 {
-    [SerializeField] Image imgGame; // 난 이거 수정
+    [SerializeField] Image imgGame;
 	[SerializeField] TextMeshProUGUI txtGameName;
 
 	public int GameIndex { get; set; }
 	public Action<int> callbackExecGame = null;
-	public Action<int, bool> callbackNormalMsg = null;
-	public Action<int> callbackErrorMsg = null;
 
 	public void ShowInfo(string gameName, Sprite gameImage)
 	{
@@ -30,39 +28,5 @@ public class GameFrame : MonoBehaviour
 	{
 		if (callbackExecGame != null)
 			callbackExecGame(GameIndex);
-	}
-
-	// 좋아요
-	public void OnClick_Good()
-	{
-		string keyName = SystemInfo.deviceName + "_good";
-		int goodValue = PlayerPrefs.GetInt(keyName, 0);
-		if(goodValue == 0)
-		{
-			Debug.Log("## 좋아요~~~");
-			PlayerPrefs.SetInt(keyName, GameIndex);
-			if (callbackNormalMsg != null)
-				callbackNormalMsg(GameIndex, true);
-		}
-		else
-		{
-			if (goodValue == GameIndex)
-			{
-				Debug.Log("## 이젠 싫어요~~~");
-				PlayerPrefs.SetInt(keyName, 0);
-				if (callbackNormalMsg != null)
-					callbackNormalMsg(GameIndex, false);
-			}
-			else
-			{
-				Debug.Log("## 이미 다른 게임에 기록했다~~~~");
-				if (callbackErrorMsg != null)
-					callbackErrorMsg(goodValue);
-			}
-		}
-
-
-
-
 	}
 }
